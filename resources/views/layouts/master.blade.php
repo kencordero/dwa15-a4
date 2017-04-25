4d70bd78959017ab940a5b279b895782f77d3196
Kenneth Cordero <kencordero@gmail.com>
2017-04-25 15:47:35 -0500

    skeleton for Welcome page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title')</title>
+
+	<meta charset='utf-8'>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css" >
+    @stack('head')
+</head>
+<body>
+	<header>
+		@yield('header')
+		@include('layouts.nav')
+	</header>
+
+	<section>
+		@yield('content')
+	</section>
+
+	<div class="navbar-fixed-bottom">
+	<hr>
+	<footer class="text-center">
+		&copy; {{ date('Y') }} Kenneth Cordero
+	</footer>
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    @stack('body')
+</body>
+</html>

7b2eade1f3fb2b3981d23198e5af2cfc91c9381a
Kenneth Cordero <kencordero@gmail.com>
2017-04-25 22:38:49 -0500

    database changes

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Foostore\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = json_decode(file_get_contents(database_path().'/data/products.json'), true);
+
+        foreach ($products as $product) {
+            Product::create([
+                'name' => $product['name'],
+                'price' => $product['price'],
+                'image_file' => $product['image_file'],
+                'description' => $product['description'] ?? null,
+            ]);
+        }
+    }
+}

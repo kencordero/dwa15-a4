b4c786f76cecb844dc7ad3cbf7d55b79b7b0a2b3
Kenneth Cordero <kencordero@gmail.com>
2017-05-10 21:20:29 -0500

    update Product Detail page

--- resources/views/products/showProduct.blade.php
+++ resources/views/products/showProduct.blade.php
@@ -5,17 +5,24 @@
 @endsection
 
 @section('content')
-    <h3 class="text-center">{{ $product->name }}</h3>
-    <img src="/images/{{ $product->image_file }}">
+    <div class="row">
+        <h3 class="col-md-6 text-center">{{ $product->name }}</h3>
+    </div>
+    <div class="col-md-6">
+        <img src="/images/{{ $product->image_file }}">
+    </div>
+    <br>
     <form method="post" action="/cart">
         {{ csrf_field() }}
         <input type="hidden" name="productId" value="{{ $product->id }}">
         <input type="submit" class="btn btn-primary" value="Add to cart">
     </form>
+    <br>
     <form method="post" action="/wishlist">
         {{ csrf_field() }}
         <input type="hidden" name="productId" value="{{ $product->id }}">
         <input type="submit" class="btn btn-success" value="Add to wishlist">
     </form>
+    <br>
     <div>${{ number_format($product->price, 2) }}</div>
 @endsection

f3c3a84b13713e6f0d7dfecd9ff4852fa0c0b684
Kenneth Cordero <kencordero@gmail.com>
2017-05-11 16:24:43 -0500

    updated home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,17 @@
     Foostore
 @endsection
 
+@push('head')
+    <link rel="stylesheet" href="/css/welcome.css" >
+@endpush
+
 @section('content')
-    <h1>Foostore</h1>
+    <h1>Welcome to</h1>
+    <div>
+        <img id="brand-image" src="/images/fs_400x100.gif" alt="FooStore brand image" >
+    </div>
     <h4>The online destination for all* of your shopping needs</h4>
-    <i>*Three</i>
+    <sub><i>*three</i></sub>
 @endsection
+
+
